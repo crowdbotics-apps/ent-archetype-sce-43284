086ceb8a6bd9c2756a6521315ec1064048c2e4eb
Crowdbotics <team@crowdbotics.com>
2023-09-08 14:02:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ent-archetype-sce-43284",
+  url: "https://ent-archetype-sce-43284.botics.co",
+  api: "https://ent-archetype-sce-43284.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
