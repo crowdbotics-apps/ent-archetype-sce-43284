b9f23e5e165eaa6b0309d198a72fa6dff4b819f1
crowdbotics-apps <team@crowdbotics.com>
2023-09-26 18:33:39 +0000

    Update models

--- backend/oda/models.py
+++ backend/oda/models.py
@@ -3,5 +3,13 @@ from django.db import models
 class Event(models.Model):
     'Generated Model'
     event_id = models.BigIntegerField()
+    event_type = models.TextField(null=True,blank=True,)
+    date_time = models.DateTimeField(null=True,blank=True,)
+    severity = models.TextField(null=True,blank=True,)
+    source = models.TextField(null=True,blank=True,)
+    title = models.TextField(null=True,blank=True,)
+    description = models.TextField(null=True,blank=True,)
+    event_handler = models.TextField(null=True,blank=True,)
+    disposition = models.TextField(null=True,blank=True,)
 
 # Create your models here.
